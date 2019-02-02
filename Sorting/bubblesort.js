1067dc6e643b4d85018d3a29c112539e759db88c
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-02 15:50:22 -0500

    added logic to break out of loops when there is no mre swapping - to eliminate unneeded loping/checking

--- Sorting/bubblesort.js
+++ Sorting/bubblesort.js
@@ -17,9 +17,29 @@
 
 // More Efficient solution
  //with this solution we run the comparisons fewer times
+// function bubbleSort(arr) {
+//   // i starts at 4
+//   for (var i = arr.length; i > 0; i--) {
+//     for (var j = 0; j < i - 1; j++) {
+//       console.log(arr, arr[j], arr[j+1]);
+//       if(arr[j] > arr[j+1]) {
+//         // SWAP
+//         var temp = arr[j];
+//         arr[j] = arr[j+1];
+//         arr[j+1] = temp;
+//       }
+//     }
+//   }
+//   // return arr;
+//   console.log(arr)
+// }
+
+// optimized with nowaps - if there arent anymore swaps then break out of loops
 function bubbleSort(arr) {
+  var noSwaps;
   // i starts at 4
   for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (var j = 0; j < i - 1; j++) {
       console.log(arr, arr[j], arr[j+1]);
       if(arr[j] > arr[j+1]) {
@@ -27,11 +47,13 @@ function bubbleSort(arr) {
         var temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
+        noSwaps = false // if we don't swap anything we never set noSwaps to false so we break
       }
     }
   }
   // return arr;
   console.log(arr)
 }
+
 
 bubbleSort([2,34,1,78,19,45])
