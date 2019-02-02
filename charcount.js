2e984e025344e77111fab424fadb94f4c71702ab
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-02 13:43:26 -0500

    Algo Practice

---
+++ charcount.js
@@ -0,0 +1,17 @@
+function countChars(str) {
+
+  var result = {};
+
+  for (var i=0; i< str.length; i++){
+    var char = str[i];
+
+    if(result[char] >0){
+      result[char]++;
+    } else{
+      result[char] = 1;
+    }
+  }
+  console.log(result)
+}
+
+countChars("hello")
