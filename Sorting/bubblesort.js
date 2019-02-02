aa11f36faa1f7e2c76d816ec9c6bfb823939aa54
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-02 15:14:11 -0500

    more efficient bubble sort algo

--- Sorting/bubblesort.js
+++ Sorting/bubblesort.js
@@ -1,6 +1,27 @@
+//Most Inefficient solution
+
+// function bubbleSort(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr.length; j++) {
+//       if(arr[j] > arr[j+1]) {
+//         // SWAP
+//         var temp = arr[j];
+//         arr[j] = arr[j+1];
+//         arr[j+1] = temp;
+//       }
+//     }
+//   }
+//   // return arr;
+//   console.log(arr)
+// }
+
+// More Efficient solution
+ //with this solution we run the comparisons fewer times
 function bubbleSort(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
+  // i starts at 4
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j+1]);
       if(arr[j] > arr[j+1]) {
         // SWAP
         var temp = arr[j];
