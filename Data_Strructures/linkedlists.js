173ba669609337a25a367d592c1de581f47b687d
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-09 09:50:53 -0500

    added code to linked list class to make newly added node the new head node

--- Data_Strructures/linkedlists.js
+++ Data_Strructures/linkedlists.js
@@ -6,5 +6,19 @@ class Node {
 }
 
 class LinkedList {
+  constructor () {
+    this.head = null
+  }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+
+    /*
+     Above can be condensed to:
+     this.head = new Node(data, this.head)
+    */
+
+  }
 
 }
